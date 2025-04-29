bc50a591e62a0d442905336931f778e54791ef3a
Petya Ignatova <58555417+Petya1551@users.noreply.github.com>
2025-04-29 19:56:16 +0300

    Delete DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Spell.java

--- DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Spell.java
+++
@@ -1,8 +0,0 @@
-package bg.tu_varna.sit.a1.f23621639.project;
-
-public class Spell extends Item{
-    public Spell(String name, int bonus) {
-        super(name, bonus);
-        this.type = "spell";
-    }
-}
