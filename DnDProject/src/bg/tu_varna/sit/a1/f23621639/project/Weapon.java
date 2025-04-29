ac30d75763dc0b9ab69e6a674550d595b4f162dc
Petya Ignatova <58555417+Petya1551@users.noreply.github.com>
2025-04-29 19:55:54 +0300

    Delete DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Weapon.java

--- DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Weapon.java
+++
@@ -1,8 +0,0 @@
-package bg.tu_varna.sit.a1.f23621639.project;
-
-public class Weapon extends Item{
-    public Weapon(String name, int bonus) {
-        super(name, bonus);
-        this.type = "weapon";
-    }
-}
