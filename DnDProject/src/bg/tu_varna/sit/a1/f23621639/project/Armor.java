518bc4b8a45ad7072269324ce46d894dbf0464fc
Petya Ignatova <58555417+Petya1551@users.noreply.github.com>
2025-04-29 19:53:01 +0300

    Delete DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Armor.java

--- DnDProject/src/bg/tu_varna/sit/a1/f23621639/project/Armor.java
+++
@@ -1,8 +0,0 @@
-package bg.tu_varna.sit.a1.f23621639.project;
-
-public class Armor extends Item{
-    public Armor(String name, int bonus) {
-        super(name, bonus);
-        this.type = "armor";
-    }
-}
